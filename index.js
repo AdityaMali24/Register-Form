050a7190f76cac9e11fb71758638098d5b802a79
AdityaMali24 <aditya.s.mali2@gmail.com>
2023-10-22 22:20:06 +0530

    deploy

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.listen(PORT, ()=>{
 
 var corsOptions = {
     origin: ["*" ,"http://localhost:3000"],
+    credentials:true,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 app.use(cors(corsOptions))
